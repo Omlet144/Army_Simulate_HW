1dc81de2c4f6ce758223d1286a9d081ca2b07867
Omlet144 <alexblack144@gmail.com>
2021-07-18 20:57:19 +0300

    add children: - C_Armor_Metal_helmet - C_Armor_Metal_corps - C_Armor_Metal_legs
    - C_Armor_Rag_helmet
    - C_Armor_Rag_corps
    - C_Armor_Rag_legs

---
+++ Army_Simulate_HW_armor/Army_Simulate_HW_armor/C_Armor_Metal_helmet.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "C_Armor_Metal.h"
+class C_Armor_Metal_helmet :
+    public C_Armor_Metal
+{
+    void take_a_hit(int take_a_hit_on_the_armor) { std::cout << "Metal armor take a hit: " << take_a_hit_on_the_armor << "\n"; }
+    void  breack(int) { std::cout << "Metal armor breack: " << break_armor << "\n"; }
+};
+
---
+++ Army_Simulate_HW_armor/Army_Simulate_HW_armor/C_Armor_Rag_helmet.cpp
@@ -0,0 +1,1 @@
+#include "C_Armor_Rag_helmet.h"
